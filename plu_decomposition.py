52d33d8bb97b30d9f20a2394ca5febb3ad79e82b
Nishanta Baral <nishantabaral48@gmail.com>
2025-10-04 23:01:58 -0700

    Fixed bugs

--- plu_decomposition.py
+++ plu_decomposition.py
@@ -10,19 +10,10 @@ def plu_decomposition_in_place(A):
         P,A,L,U = paqlu_square.paqlu_decomposition_in_place(A)
         p = P.argmax(axis=1)   # row-wise argmax -> row permutation
         q = Q.argmax(axis=0)
-        return p,q,A
+        return p,q
     else:
         P,Q,L,U = paqlu_rectangular.paqlu_decomposition_in_place(A)
         p = P.argmax(axis=1)   # row-wise argmax -> row permutation
         q = Q.argmax(axis=0)   # col-wise argmax -> col permutation
-        return p,q,A
+        return p,q
     
-A = np.array([[5,2,1],
-              [10,7,6],
-              [-5,7,13],
-              [0,6,10]], dtype=float)
-p,q,A = plu_decomposition_in_place(A)
-print("p is", p)
-print("q is", q)
-print("PAQ is", A)
-print("PAQ is", A[np.ix_(p,q)])
