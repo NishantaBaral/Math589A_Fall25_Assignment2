4e4a424ea45c8bb8098bf37b154ce47fce0333f3
Nishanta Baral <nishantabaral48@gmail.com>
2025-10-06 18:44:51 -0700

    Fixed errors

--- general_linear_solver.py
+++ general_linear_solver.py
@@ -36,7 +36,7 @@ def square_solver(A, b):
     x_perm = back_substitution(U, y)  # solve Ux' = y
     x = np.dot(Q, x_perm)         # unpermute x' according to Q
     nullspace = np.zeros((n, 0), dtype=float)
-    return x, nullspace
+    return nullspace, x
 
 def rectangular_solver(A, b):
     m,n = A.shape
@@ -71,4 +71,4 @@ def rectangular_solver(A, b):
     else:
         nullspace = np.zeros((n, 0), dtype=float)
 
-    return x_particular, nullspace
+    return nullspace, x_particular
